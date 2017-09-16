a73084636fcc0e84f05b17d02915e4a72b58bdab
Tatsiana Bukzheleva <ledimersedes@yandex.ru>
2017-09-16 12:04:06 +0300

    18 is solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+
+var arr = expression.split('*');
+
+var n2 = 0;
+var n5 = 0;
+
+function f1(x){
+   for(var i=x; i>=1; i -=1){
+     var h = i;
+    while(i%5===0){
+      n5++;
+      i=i/5;
+    };
+    while(i%2===0){
+      n2++;
+      i=i/2;
+    };
+    i = h;
+   };
+  };
+
+function f2(x){
+   for(var i=x; i>=2; i -=2){
+    var h = i;
+    while(i%2===0){
+      n2++;
+      i=i/2;
+    };
+    while(i%5===0){
+      n5++;
+      i=i/5;
+    };
+    i = h;
+   };
+
+  }; 
+
+  for(var j=0; j<arr.length; j++){
+    var find = arr[j].search(/!!/);
+    if(find){
+      var num = arr[j].slice(0,-2);
+      f2(num); };
+
+    var find2 = arr[j].search(/!/);
+    if(find2){
+      var num = arr[j].slice(0,-1);
+      f1(num);
+    };
+  };
+return Math.min(n2,n5);
+};
