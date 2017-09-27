9b7d12a237b461863cb265c3777860d3093c4cd7
Tatsiana Bukzheleva <ledimersedes@yandex.ru>
2017-09-27 08:51:23 +0300

    change last for loop

--- src/index.js
+++ src/index.js
@@ -1,52 +1,55 @@
 module.exports = function zeros(expression) {
 
-var arr = expression.split('*');
-
-var n2 = 0;
-var n5 = 0;
+let arr = expression.split('*');
+  let n2 = 0;
+  let n5 = 0;
 
 function f1(x){
-   for(var i=x; i>=1; i -=1){
-     var h = i;
+   for(let i=x; i>=2; i -=1){
+     let h = i;
     while(i%5===0){
-      n5++;
       i=i/5;
+      n5++;
     };
+    i = h;
     while(i%2===0){
-      n2++;
       i=i/2;
+      n2++;
     };
     i = h;
    };
   };
 
 function f2(x){
-   for(var i=x; i>=2; i -=2){
-    var h = i;
+   for(let i=x; i>=2; i -=2){
+    let h = i;
     while(i%2===0){
-      n2++;
       i=i/2;
+      n2++;
     };
+    i = h;
     while(i%5===0){
-      n5++;
       i=i/5;
+      n5++;
     };
     i = h;
    };
-
   }; 
 
-  for(var j=0; j<arr.length; j++){
-    var find = arr[j].search(/!!/);
-    if(find){
-      var num = arr[j].slice(0,-2);
-      f2(num); };
-
-    var find2 = arr[j].search(/!/);
-    if(find2){
-      var num = arr[j].slice(0,-1);
-      f1(num);
-    };
+  for(let j=0; j<arr.length; j++){
+    let find1 = arr[j].search(/!/);
+    let find2 = arr[j].search(/!!/);
+    if(find2>-1){
+      let num2 = arr[j].slice(0,-2);
+      f2(num2);
+    }
+    if(find1>-1){
+      let num1 = arr[j].slice(0,-1);
+      f1(num1);
+    }
   };
-return Math.min(n2,n5);
+
+
+return Math.min(n5,n2);
+
 };
